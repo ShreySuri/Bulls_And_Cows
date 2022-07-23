8c2b1d492503687c21cedb5ef173923b7d9b044b
ShreySuri <shreysuri5@gmail.com>
2022-07-22 19:51:19 -0500

    Update Mastermind_1.py

--- Mastermind_1.py
+++ Mastermind_1.py
@@ -9,3 +9,15 @@ for i in range (0, 4):
 	num_list.append(y)
 	known_list.append("X ")
 
+game = True
+
+while game == True:
+	
+	input_1 = 0
+	while input_1 % 1 != 0 or input_1 < 1 or input_1 > 9999:
+		print("")
+		input_1 = input(print("Enter a 4 digit  number. "))
+		input_1 = float(input_1)
+	user_num = int(input_1)
+
+	user_num_list = convert(user_num)
