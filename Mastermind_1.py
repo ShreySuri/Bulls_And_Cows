e484ec0c0190c650a18d5b503a8339b078732dfe
ShreySuri <shreysuri5@gmail.com>
2022-07-22 21:11:50 -0500

    Update Mastermind_1.py

--- Mastermind_1.py
+++ Mastermind_1.py
@@ -1,3 +1,29 @@
+def convert(int_1):
+	power = 1
+	list_1 = []
+	while int_1 > 10 ** power:
+		power = power + 1
+	while power >= 0:
+		divisor = 10 ** power
+		rem = int_1 % divisor
+		quotient = int((int_1 - rem)/divisor)
+		list_1.append(quotient)
+		int_1 = rem
+		power = power - 1
+	return(list_1)
+
+
+
+
+
+
+
+
+
+
+
+
+
 import random
 
 num_list = []
