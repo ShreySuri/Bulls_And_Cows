e4b689a292bf0719494323402bc2e8ceceb5b2e5
ShreySuri <shreysuri5@gmail.com>
2022-07-30 17:13:16 -0500

    Update test_file_6.py

--- test_file_6.py
+++ test_file_6.py
@@ -1,16 +1,16 @@
 def convert(int_1):
-	power = 1
-	list_1 = []
-	while int_1 >= 10 ** power:
-		power = power + 1
-	while power > 0:
-		power = power - 1
-		divisor = 10 ** power
-		rem = int_1 % divisor
-		quotient = int((int_1 - rem)/divisor)
-		list_1.append(quotient)
-		int_1 = rem
-	return(list_1)
+        power = 1
+        list_1 = []
+        while int_1 >= 10 ** power:
+                power = power + 1
+        while power > 0:
+                power = power - 1
+                divisor = 10 ** power
+                rem = int_1 % divisor
+                quotient = int((int_1 - rem)/divisor)
+                list_1.append(quotient)
+                int_1 = rem
+        return(list_1)
 
 def listToString(s):
     str1 = "" 
@@ -38,30 +38,31 @@ while game == True:
 		print("")
 		input_1 = input(print("Enter a 4 digit  number. "))
 		input_1 = float(input_1)
+
 	user_num = int(input_1)
 
 	user_num_list = convert(user_num)
 
 	cows = 0
-        bulls = 0
+	bulls = 0
 
 	for i in range (0, 4):
-                for j in range (0, 4):
+		for j in range (0, 4):              
                         if user_num_list[i] == num_list[j]:
                                 cows = cows + 1
                         else:
                                 toggle = True
-
-        for k in range (0, 4):
+	
+	for k in range (0, 4):
                 if user_num_list[k] == num_list[k]:
                         bulls = bulls + 1
                 else:
                         bulls = bulls + 0
 
-        cows = cows - bulls
+	cows = cows - bulls
 
-        print("")
-        print("%s Bulls, %s Cows" % (bulls, cows))
+	print("")
+	print("%s Bulls, %s Cows" % (bulls, cows))
 
 print("")
 print("You won! It took you %s guesses. " % turn_count)
