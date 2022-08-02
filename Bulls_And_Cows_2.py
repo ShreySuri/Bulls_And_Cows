dab5fb16c2ae5ba833757929bae3c173ddad489c
ShreySuri <shreysuri5@gmail.com>
2022-08-02 08:58:59 -0500

    Update Bulls_And_Cows_2.py

--- Bulls_And_Cows_2.py
+++ Bulls_And_Cows_2.py
@@ -1,16 +1,3 @@
-def convert(int_1):
-     power = 1
-     list_1 = []
-     while int_1 >= 10 ** power:
-          power = power + 1
-     while power > 0:
-          power = power - 1
-          divisor = 10 ** power
-          rem = int_1 % divisor
-          quotient = int((int_1 - rem)/divisor)
-          list_1.append(quotient)
-          int_1 = rem
-     return(list_1)
 
 
 import random
@@ -24,24 +11,17 @@ for i in range (0, 4):
 game = True
 
 while game == True:
-    counter = 0
-    while counter != 4:
+    user_num = 0.5
+    while user_num % 1 != 0 or user_num < 0 or user_num > 9999:
         print("")
-        user_num = input(print("Enter a 4-digit code. If you would like an example, type 'example'."))
+        user_num = input(print("Enter a 4-digit code. If you would like some examples, type 'example'. "))
         if user_num == "example":
             print("")
             for i in range (0, 10):
                 ex_list = []
                 for i in range (0, 4):
-                    x = random.randint(0, 9)
+                    x = random.randint(0, 9):
                     ex_list.append(x)
                 print(ex_list)
         else:
-            num_list = convert(float(user_num))
-            counter = 0
-            for i in range (0, 4):
-                for j in range (0, 10):
-                    if num_list[i] == j:
-                        counter = counter + 1
-                    else:
-                        toggle = True
+            user_list = list(user_num)
